b811f2b2c2568c5a4edf5f30496c5dac8ae0292b
f1w2e3 <daywedset@gmail.com>
2026-01-28 23:43:14 +0900

    axios로 변경

--- server/index.js
+++ server/index.js
@@ -26,16 +26,25 @@ app.get('/', (req, res) => {
     res.send('Movie API 서버가 실행 중입니다.');
   });
 
-app.get('/api/movies/:id', async (req, res) => {
-  const movieId = req.params.id;
-  try {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.TMDB_API_KEY}&language=ko`);
-    const data = await response.json();
-    res.json(data);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch movie details' });
-  }
-});
+  app.get('/api/movies/:id', async (req, res) => {
+    const movieId = req.params.id;
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/${movieId}`,
+        {
+          params: {
+            api_key: process.env.TMDB_API_KEY,
+            language: "ko"
+          }
+        }
+      );
+      res.json(response.data);
+    } catch (err) {
+      console.error(err.response?.data || err.message);
+      res.status(500).json({ error: 'Failed to fetch movie details' });
+    }
+  });
+  
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
