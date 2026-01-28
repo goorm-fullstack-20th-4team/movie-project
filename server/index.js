018a5928f8e48149da273984bdb8aaaa3b41def5
f1w2e3 <daywedset@gmail.com>
2026-01-28 23:18:18 +0900

    검색기능 추가

--- server/index.js
+++ server/index.js
@@ -26,4 +26,17 @@ app.get('/', (req, res) => {
     res.send('Movie API 서버가 실행 중입니다.');
   });
 
+app.get('/api/movies/:id', async (req, res) => {
+  const movieId = req.params.id;
+  try {
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.TMDB_KEY}&language=ko`);
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch movie details' });
+  }
+});
+
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
